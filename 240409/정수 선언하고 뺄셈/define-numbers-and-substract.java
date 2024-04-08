7930b683f696a146db2c7723a1c0c99161f98c35
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-09 01:31:03 +0900

    Title: 정수 선언하고 뺄셈, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240409/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 97;
+        int b = 13;
+        int ret = a - b;
+
+        System.out.println(a + " - " + b + " = " + ret);
+    }
+}

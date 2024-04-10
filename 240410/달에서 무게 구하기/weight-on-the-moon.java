ab9876e0d68e70416c5b7f8726730b29271769c4
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-10 20:52:42 +0900

    Title: 달에서 무게 구하기, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240410/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 13;
+        double b = 0.165000;
+        double ret = a * b;
+
+        System.out.printf("%.0f * %.6f = %.6f", a, b, ret);
+    }
+}

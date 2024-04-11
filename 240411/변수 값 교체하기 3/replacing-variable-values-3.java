4f57304e30f890f1600ab5d86589378c07f47ff5
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-11 15:51:14 +0900

    Title: 변수 값 교체하기 3, Time: 81ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240411/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int tmp;
+
+        tmp = a;
+        a = b;
+        b = tmp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

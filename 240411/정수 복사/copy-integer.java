d315624f8b119bfb74781a9906b430831dc1519b
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-11 15:48:54 +0900

    Title: 정수 복사, Time: 75ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240411/정수 복사/copy-integer.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        b = a;
+        int ret = a * b;
+
+        System.out.println(a + " " + b);
+        System.out.println(ret);
+
+    }
+}

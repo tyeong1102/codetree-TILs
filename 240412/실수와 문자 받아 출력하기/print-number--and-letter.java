db2bd9b02199f8894b302af0c75e54075253f23d
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-12 15:17:38 +0900

    Title: 실수와 문자 받아 출력하기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240412/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String c = br.readLine();
+        double a = Double.parseDouble(br.readLine());
+        double b = Double.parseDouble(br.readLine());
+
+        System.out.println(c.charAt(0));
+        System.out.println(String.format("%.2f", a));
+        System.out.println(String.format("%.2f", b));
+    }
+}

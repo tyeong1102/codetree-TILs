7c3bc2477e4c7ab2e8bdfd078cca297afcee27ec
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-16 16:21:03 +0900

    Title: 짝수만 출력, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240416/짝수만 출력/output-only-even.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int a, b;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = a; i <= b; i++) {
+            if (i % 2 == 0) {
+                sb.append(i).append(" ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}

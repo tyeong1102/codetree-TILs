28071fd76873f9057374521d0bbb2d21056905f7
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-16 16:18:09 +0900

    Title: 홀수만 출력 2, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240416/홀수만 출력 2/output-only-odd-2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int a, b;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        for (int i = a; i >= b; i--) {
+            if (i % 2 == 1) {
+                sb.append(i).append(" ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}

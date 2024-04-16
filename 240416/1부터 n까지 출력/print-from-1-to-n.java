789ac790a0722cd0c965b8198d72b95585052cc1
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-16 16:16:03 +0900

    Title: 1부터 n까지 출력, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240416/1부터 n까지 출력/print-from-1-to-n.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}

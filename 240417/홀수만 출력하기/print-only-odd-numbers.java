ce76667fa744ad0046c0b054c6ae1b8756379caa
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-17 19:33:37 +0900

    Title: 홀수만 출력하기, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240417/홀수만 출력하기/print-only-odd-numbers.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num % 3 == 0) {
+                sb.append(num).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}

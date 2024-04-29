2d0d76bc2798817f1f059ba5627c85b582a91d78
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-29 20:21:13 +0900

    Title: 최고의 13위치, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240429/최고의 13위치/best-place-of-13.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n;
+    public static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n - 3; j++) {
+                int cnt = 0;
+                for (int k = 0; k < 3; k++) {
+                    if (arr[i][j + k] == 1) {
+                        cnt++;
+                    }
+                }
+                if (cnt == 3) {
+                    max = cnt;
+                    return;
+                }
+                max = Math.max(max, cnt);
+            }
+        }
+
+        System.out.println(max);
+
+    }
+}

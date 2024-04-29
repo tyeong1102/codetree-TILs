0d34e8f95c600732d8b6e9593bd338b6037d744e
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-29 20:06:29 +0900

    Title: 일렬로 서있는 소 2, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240429/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 240429/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -22,9 +22,11 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                for (int k = j; k < n; k++) {
-                    if (arr[i] < arr[j] && arr[j] < arr[k]) {
-                        ret++;
+                if (arr[i] < arr[j]) {
+                    for (int k = j; k < n; k++) {
+                        if (arr[j] < arr[k]) {
+                            ret++;
+                        }
                     }
                 }
             }

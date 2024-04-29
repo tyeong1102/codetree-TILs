b9a59e8aac9f26a0dde258332a0b05d8159db712
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-29 20:04:47 +0900

    Title: 일렬로 서있는 소 2, Time: 110ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240429/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n;
+    public static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ret = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                for (int k = j; k < n; k++) {
+                    if (arr[i] < arr[j] && arr[j] < arr[k]) {
+                        ret++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(ret);
+    }
+}

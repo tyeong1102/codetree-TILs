ad6d44897a5ab20f535d3f43ed9630a1aef06eb0
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-30 16:02:07 +0900

    Title: 이상한 진수 2, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240430/이상한 진수 2/awkward-digits-2.java
+++ 240430/이상한 진수 2/awkward-digits-2.java
@@ -8,13 +8,18 @@ public class Main {
         String str = br.readLine();
 
         if (!str.contains("0")) {
-            System.out.println(Integer.parseInt(str, 2));
-            return;
+            if (str.length() == 1) {
+                System.out.println(0);
+                return;
+            } else {
+                System.out.println(Integer.parseInt(str, 2));
+                return;
+            }
         }
 
         String modified = str.replaceFirst("0", "1");
-        int ret = Integer.parseInt(modified, 2);
+        int max = Integer.parseInt(modified, 2);
 
-        System.out.println(ret);
+        System.out.println(max);
     }
 }

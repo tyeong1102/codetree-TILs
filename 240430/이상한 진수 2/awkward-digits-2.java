88773974424b4c74967b01a4ed281cbbe4057d1d
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-30 15:46:11 +0900

    Title: 이상한 진수 2, Time: 78ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240430/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        if (!str.contains("0")) {
+            System.out.println(Integer.parseInt(str, 2));
+            return;
+        }
+
+        String modified = str.replaceFirst("0", "1");
+        int max = Integer.parseInt(modified, 2);
+
+        System.out.println(max);
+    }
+}
